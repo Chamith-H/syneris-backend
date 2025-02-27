5f2178eb6d85470e1e118f6cb1c4f78f9bd0b11e
Chamith-Digitus <chamith.h@digitustecglobal.com>
2025-02-27 17:19:12 +0530

    disabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Origin,Accept,Content-Type,Authorization',
-    credentials: true,
-  };
-  app.enableCors(corsOptions);
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
